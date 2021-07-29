2b851892afdbecaab41a3fd1e780573037349d36
Rafael <rflernesto@gmail.com>
2021-07-28 23:55:45 -0400

    feat: creates findOne method on users service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
-export type User = any;
 @Injectable()
-export class AuthService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'bob',
-      password: '12345678',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: '7654321',
-    },
-  ];
-
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => username === user.username);
-  }
-}
+export class AuthService {}
